9c6fa4d079ab22b15c36b4c6dab39b4e8f5291c8
Lightwing-Ng <rodney_ng@icloud.com>
2023-07-07 14:43:30 +0800

    1. Added CRSI as an indicator; 2. Moved print_trade_records() more ahead; 3. Disabled MACD / RSI / KDJ subplots' Y axis.

--- main.py
+++ main.py
@@ -13,15 +13,46 @@ from flask import Flask, render_template, request
 
 
 def print_realtime_ratting(df):
-    print("Datetime\t\t\tDIR\t\tPrice\tRSI\t\tDIF\t\tDEM\t\tKDJ")
+    print("\nDatetime\t\t\tDIR\t\tPrice\tCRSI\tDIF\t\tDEM\t\tKDJ\t\tAO")
     for i in range(len(df)):
         current = df["BuyIndex"][i]
         if current == "Buy" or current == "PotentialBuy":
-            print("%s\t\033[31mBuy \t%.2f\033[0m\t%5.2f\t%6.2f\t%6.2f\t%6.2f" % (
-                df["Datetime"][i], df["Low"][i], df["RSI"][i], df["DIF"][i], df["DEM"][i], df["KDJ"][i]))
+            print("%s\t\033[31mBuy \t%.2f\033[0m\t%5.2f\t%6.2f\t%6.2f\t%6.2f\t%6.2f" % (
+                df["Datetime"][i],
+                df["Low"][i],
+                df["CRSI"][i],
+                df["DIF"][i],
+                df["DEM"][i],
+                df["KDJ"][i],
+                df["AO"][i]))
         elif current == "Sell" or current == "PotentialSell":
-            print("%s\t\033[34mSell\t%.2f\033[0m\t%5.2f\t%6.2f\t%6.2f\t%6.2f" % (
-                df["Datetime"][i], df["High"][i], df["RSI"][i], df["DIF"][i], df["DEM"][i], df["KDJ"][i]))
+            print("%s\t\033[34mSell\t%.2f\033[0m\t%5.2f\t%6.2f\t%6.2f\t%6.2f\t%6.2f" % (
+                df["Datetime"][i],
+                df["High"][i],
+                df["CRSI"][i],
+                df["DIF"][i],
+                df["DEM"][i],
+                df["KDJ"][i],
+                df["AO"][i]))
+
+
+def print_trade_records(df):
+    print("\nDatetime\t\t\tDIR\t\tPrice\tPSN\t\tCMS\t\tBalance\t\tTotal\t\tRemarks")
+    for i in range(len(df)):
+        direction = df["BuyIndex"][i]
+
+        if direction == "Buy" or direction == "Sell":
+            print("%s\t%-4s\t%5.2f\t%5d\t%6.2f\t%10s\t%10s\t%s" % (
+                df["Datetime"][i],
+                df["BuyIndex"][i],
+                df["Low"][i] if df["BuyIndex"][i] == "Buy" else df["High"][i],
+                df["Position"][i] if df["Position"][i] > 0 else df["Position"][i - 1],
+                df["Commission"][i],
+                f"{df['Balance'][i]:,.2f}",
+                f"{df['TotalAssets'][i]:,.2f}",
+                df["Remarks"][i]))
+
+    return df["TotalAssets"][len(df) - 1]
 
 
 def generate_US_trade_days(start_date, end_date):
@@ -67,8 +98,7 @@ def find_signals(df):
 
         DIF = df["DIF"][i]
         DEM = df["DEM"][i]
-        Histogram = df["Histogram"][i]
-        RSI = df["RSI"][i]
+        CRSI = df["CRSI"][i]
         K = df["K"][i]
         D = df["D"][i]
         J = df["J"][i]
@@ -77,9 +107,9 @@ def find_signals(df):
             DIF_last = df["DIF"][i - 1]
             DEM_last = df["DEM"][i - 1]
 
-            if (DIF > DEM and DIF_last < DEM_last) and RSI <= 65 and (J >= K and J >= D):
+            if (DIF > DEM and DIF_last < DEM_last) and (J >= K and J >= D) and CRSI <= 70:
                 df.iloc[i, df.columns.get_loc("BuyIndex")] = "PotentialBuy"
-            elif (DIF < DEM and DIF_last > DEM_last) and RSI >= 45 and (J <= K and J <= D):
+            elif (DIF < DEM and DIF_last > DEM_last) and (J <= K and J <= D) and CRSI >= 40:
                 df.iloc[i, df.columns.get_loc("BuyIndex")] = "PotentialSell"
             else:
                 df.iloc[i, df.columns.get_loc("BuyIndex")] = "Hold"
@@ -120,7 +150,7 @@ def print_trade(df, principal):
         df.iloc[i, df.columns.get_loc("Cost")] = 0
 
     take_profit_limit = 0.005
-    stop_loss_limit = 0.01
+    stop_loss_limit = 0.005
     df["Balance"] = principal
     df["Position"] = 0
     df["Commission"] = 0.00
@@ -159,25 +189,6 @@ def print_trade(df, principal):
     return df
 
 
-def print_trade_records(df):
-    print("\nDatetime\t\t\tDIR\t\tPrice\tPSN\t\tCMS\t\tBalance\t\tTotal\t\tRemarks")
-    for i in range(len(df)):
-        direction = df["BuyIndex"][i]
-
-        if direction == "Buy" or direction == "Sell":
-            print("%s\t%-4s\t%5.2f\t%5d\t%6.2f\t%10s\t%10s\t%s" % (
-                df["Datetime"][i],
-                df["BuyIndex"][i],
-                df["Low"][i] if df["BuyIndex"][i] == "Buy" else df["High"][i],
-                df["Position"][i] if df["Position"][i] > 0 else df["Position"][i - 1],
-                df["Commission"][i],
-                f"{df['Balance'][i]:,.2f}",
-                f"{df['TotalAssets'][i]:,.2f}",
-                df["Remarks"][i]))
-
-    return df["TotalAssets"][len(df) - 1]
-
-
 def plot_vertical_lines(df, ax):
     for i in range(len(df)):
         x_trade = df["Datetime"][i]
@@ -193,28 +204,28 @@ def mark_buy_and_sell(df, ax):
         x_trade = df["Datetime"][i]
         y_trade = -100
         if df["BuyIndex"][i] == "Buy":
-            text = "B\n" + f"{df['Low'][i]:,.2f}"
+            text = "Buy\n" + f"{df['Low'][i]:,.2f}"
             ax.annotate(text, xy=(x_trade, y_trade), xytext=(
                 x_trade, y_trade), color="#ffffff", fontsize=8,
-                        bbox=dict(boxstyle="round, pad=0.15, rounding_size=0.15", facecolor="#ff2f92",
+                        bbox=dict(boxstyle="round, pad=0.15, rounding_size=0.25", facecolor="#ff2f92",
                                   edgecolor="none", alpha=1))
         elif df["BuyIndex"][i] == "PotentialBuy":
             text = f"{df['Low'][i]:,.2f}"
             ax.annotate(text, xy=(x_trade, y_trade), xytext=(
                 x_trade, y_trade), color="#ffffff", fontsize=7,
-                        bbox=dict(boxstyle="round, pad=0.15, rounding_size=0.15", facecolor="#ff2f92",
+                        bbox=dict(boxstyle="round, pad=0.15, rounding_size=0.25", facecolor="#ff2f92",
                                   edgecolor="none", alpha=1))
         elif df["BuyIndex"][i] == "Sell":
-            text = "S\n" + f"{df['High'][i]:,.2f}"
+            text = "Sell\n" + f"{df['High'][i]:,.2f}"
             ax.annotate(text, xy=(x_trade, y_trade), xytext=(
                 x_trade, y_trade + 80), color="#ffffff", fontsize=8,
-                        bbox=dict(boxstyle="round, pad=0.15, rounding_size=0.15", facecolor="#0055cc",
+                        bbox=dict(boxstyle="round, pad=0.15, rounding_size=0.25", facecolor="#0055cc",
                                   edgecolor="none", alpha=1))
         elif df["BuyIndex"][i] == "PotentialSell":
             text = f"{df['High'][i]:,.2f}"
             ax.annotate(text, xy=(x_trade, y_trade), xytext=(
                 x_trade, y_trade), color="#ffffff", fontsize=7,
-                        bbox=dict(boxstyle="round, pad=0.15, rounding_size=0.15", facecolor="#0055cc",
+                        bbox=dict(boxstyle="round, pad=0.15, rounding_size=0.25", facecolor="#0055cc",
                                   edgecolor="none", alpha=1))
 
 
@@ -230,6 +241,7 @@ def plot_candlestick(df, ax, ticker):
     [ax.spines[s].set_visible(False) for s in ["top", "right", "bottom", "left"]]
     ax.set_xticklabels([])
     ax.set_xticks([])
+    ax.tick_params(axis="y", labelsize=7, labelcolor="#0055cc", length=0)
     ax.margins(x=0)
 
 
@@ -242,9 +254,11 @@ def plot_MACD(df, ax, date_format):
     ax.xaxis.set_ticks_position("top")
     ax.yaxis.set_label_position("right")
     ax.yaxis.set_ticks_position("right")
+    ax.set_yticklabels([])
+    ax.set_yticks([])
     [ax.spines[s].set_visible(False) for s in ["top", "right", "bottom", "left"]]
     ax.xaxis.set_major_formatter(date_format)
-    ax.tick_params(axis="x", top=False)
+    ax.tick_params(axis="x", labelsize=7, labelcolor="#0055cc", top=False)
     ax.plot(df["Datetime"], df["DIF"], color="#0055cc", label="DIF", linewidth=1)
     ax.plot(df["Datetime"], df["DEM"], color="#ffa500", label="DEM", linewidth=1)
     ax.bar(df["Datetime"], df["Histogram"], width=[0.0005 if len(df) <= 390 else 2000 / len(df)],
@@ -253,16 +267,18 @@ def plot_MACD(df, ax, date_format):
 
 def plot_RSI(df, ax):
     # plot the RSI on ax
-    ax.set_ylabel("RSI")
+    ax.set_ylabel("CRSI")
     ax.set_xlim(min(df["Datetime"]), max(df["Datetime"]))
     ax.margins(x=0)
     ax.yaxis.set_label_position("right")
     ax.yaxis.set_ticks_position("right")
-    ax.plot(df["Datetime"], df["RSI"], label="RSI", color="#0055cc", linewidth=1)
-    # ax.plot(df["Datetime"], df["CCI"] / 4, label="CCI", color="#ff2f92", linewidth=1)
+    ax.plot(df["Datetime"], df["CRSI"], label="CRSI", color="#0055cc", linewidth=1)
+    ax.plot(df["Datetime"], df["RSI"], label="RSI", color="#ff2f92", linewidth=1, alpha=0.5)
     [ax.spines[s].set_visible(False) for s in ["top", "right", "bottom", "left"]]
     ax.set_xticklabels([])
     ax.set_xticks([])
+    ax.set_yticklabels([])
+    ax.set_yticks([])
     ax.set_ylim(0, 100)
 
 
@@ -279,6 +295,8 @@ def plot_KDJ(df, ax):
     [ax.spines[s].set_visible(False) for s in ["top", "right", "bottom", "left"]]
     ax.set_xticklabels([])
     ax.set_xticks([])
+    ax.set_yticklabels([])
+    ax.set_yticks([])
     ax.set_ylim(-100, 200)
 
 
@@ -293,6 +311,8 @@ def plot_Volume(df, ax):
     ax.set_ylim(0, max(df["Volume"]))
     ax.set_xticklabels([])
     ax.set_xticks([])
+    ax.set_yticklabels([])
+    ax.set_yticks([])
 
     # Add a smooth fitting line based on df["Volume"]
     x_new = pd.date_range(df["Datetime"].min(), df["Datetime"].max(), periods=300)
@@ -309,14 +329,21 @@ def calculate_df(df):
     df["DIF"] = ta.trend.MACD(df["Close"], window_slow=26, window_fast=12).macd()
     df["DEM"] = df["DIF"].ewm(span=9).mean()
     df["Histogram"] = df["DIF"] - df["DEM"].ewm(span=9).mean()
-
     df["KDJ"] = ta.momentum.StochasticOscillator(df["High"], df["Low"], df["Close"]).stoch()
-    df["RSI"] = ta.momentum.RSIIndicator(df["Close"], window=14).rsi()
     df["K"] = ta.momentum.StochasticOscillator(df["High"], df["Low"], df["Close"], window=9).stoch()
     df["D"] = df["K"].ewm(com=2).mean()
     df["J"] = 3 * df["K"] - 2 * df["D"]
 
+    df["RSI"] = ta.momentum.RSIIndicator(df["Close"], window=14).rsi()
+    df["CRSI"] = (df["RSI"] + 2 * df["RSI"].shift(1) + 3 * df["RSI"].shift(2) + 4 * df["RSI"].shift(3)) / 10
+
     df["CCI"] = ta.trend.CCIIndicator(df["High"], df["Low"], df["Close"], window=20, constant=0.015).cci()
+    df["AO"] = df[["High", "Low"]].mean(axis=1).rolling(5).mean().sub(
+        df[["High", "Low"]].mean(axis=1).rolling(34).mean())
+    df["SMA"] = df["Close"].rolling(20).mean()
+    df["STD"] = df["Close"].rolling(20).std()
+    df["Upper Band"] = df["SMA"].add(df["STD"].mul(2))
+    df["Lower Band"] = df["SMA"].sub(df["STD"].mul(2))
 
     return df
 
@@ -325,7 +352,7 @@ def plot_stock_screener(df, ticker, type):
     # Plot stock price, MACD, KDJ, RSI using matplotlib
     file_name = ""
     plt.rcParams["font.family"] = "Menlo"
-    fig = plt.figure(figsize=(16, 9), dpi=300)
+    fig = plt.figure(figsize=(20, 9), dpi=300)
 
     ax1 = plt.subplot2grid((9, 1), (0, 0), rowspan=4)
     ax2 = plt.subplot2grid((9, 1), (4, 0), rowspan=2)
@@ -359,7 +386,7 @@ def plot_stock_screener(df, ticker, type):
         plot_vertical_lines(df, ax5)
 
     # save the figure
-    fig.savefig(file_name, transparent=True, bbox_inches="tight")
+    fig.savefig(file_name, transparent=False, bbox_inches="tight")
 
 
 def plotOneDay(ticker, start_time, end_time):
@@ -386,7 +413,7 @@ def plotOneMinute(ticker, trade_day):
     return df
 
 
-def print_all_stocks(trade_day):
+def print_all_stocks(trade_day, principal):
     # For all stocks in the list
     for ticker in tickers:
         now = datetime.now()
@@ -405,9 +432,9 @@ def print_all_stocks(trade_day):
         plot_stock_screener(df, ticker, "1d")
 
 
-def print_stock_recent(ticker, principal):
+def print_stock_recent(ticker, start_date, end_date, principal):
     # For a stock in recent 30 days
-    trade_days = generate_US_trade_days("2023-06-06", "2023-07-05")
+    trade_days = generate_US_trade_days(start_date, end_date)
 
     for i in trade_days:
         trade_day = str(i)[:10]
@@ -416,13 +443,12 @@ def print_stock_recent(ticker, principal):
         print_realtime_ratting(df)
         principal = print_trade_records(df)
 
-    print("Total: %10s" % f"{principal:,.2f}")
+    print("\nTotal: %10s" % f"{principal:,.2f}")
 
 
 tickers = [
     # "2800.hk", "0005.hk", "0700.hk", "2388.hk", "2888.hk",
-    "MSFT", "NVDA",
-    "TSM", "GOOGL", "META",
+    "MSFT", "NVDA", "TSM", "GOOGL", "META",
     "ORCL", "AMZN", "QCOM", "AMD", "VZ",
     "NFLX", "ASML", "JPM", "GS", "MS",
     "WFC", "BAC", "V", "MA", "AXP",
@@ -434,8 +460,8 @@ date_string = today.strftime("%Y-%m-%d")
 date_string_today = today.strftime("%Y-%m-%d")
 principal = 10000
 
-# print_all_stocks("2023-07-06")
-# print_stock_recent("NVDA", principal)
+# print_all_stocks("2023-06-28", principal)
+# print_stock_recent("AMD", "2020-01-01", "2023-07-06",principal)
 
 # Start of web
 app = Flask(__name__, template_folder="template")
@@ -446,7 +472,7 @@ def handle_query():
     if request.method == "POST":
 
         trade_date = request.form["trade_date"]
-        res = np.array([["APPL", 0.00, 0.00, 0.00, 0.00, 0.00, "", ""]])
+        res = np.array([["APPL", 0.00, 0.00, 0.00, 0.00, 0.00, 0.00, "", ""]])
 
         for ticker in tickers:
 
@@ -472,7 +498,8 @@ def handle_query():
                 ticker,
                 f"{df['DIF'][len(df) - 1]:,.2f}",
                 f"{df['DEM'][len(df) - 1]:,.2f}",
-                f"{df['RSI'][len(df) - 1]:,.2f}",
+                f"{df['CRSI'][len(df) - 1]:,.2f}",
+                f"{df['KDJ'][len(df) - 1]:,.2f}",
                 f"{df['CCI'][len(df) - 1]:,.2f}",
                 f"{df['Close'][len(df) - 1]:,.2f}",
                 f"%s\n%s" % (buyTime, f"{buyPrice:,.2f}"),
