47dbda6e9caa951fe53fafa12f0d4a912c941e21
Lightwing-Ng <rodney_ng@icloud.com>
2023-07-18 17:03:31 +0800

    Changed the last day to update

--- app/views.py
+++ app/views.py
@@ -117,19 +117,22 @@ def email_notification(ticker, interval, email):
         analysis = handler.get_analysis()
 
         today_string = datetime.now().strftime("%Y-%m-%d")
+        print(ticker, today_string, interval, md.interval_type.get(interval))
         df = md.get_df_interval(ticker, today_string, interval, md.interval_type.get(interval))
 
-        signal_buy, price_buy = False, 0.00
-        signal_sell, price_sell = False, 0.00
+        signal_buy, price_buy, datetime_buy = False, 0.00, ""
+        signal_sell, price_sell, datetime_sell = False, 0.00, ""
         price_close = analysis.indicators["close"]
         recent_cycle = 26
 
+        # print(f"{datetime.now()} {ticker} {interval} {analysis.summary['RECOMMENDATION']}")
+
         for i in range(len(df) - 1, max(len(df) - recent_cycle, -1), -1):
-            current, price = df["BuyIndex"][i], df["Close"][i]
+            current, price, datetime_last = df["BuyIndex"][i], df["Close"][i], df["Datetime"][i]
             if current == "PotentialBuy" and not signal_buy:
-                signal_buy, price_buy = True, price
+                signal_buy, price_buy, datetime_buy = True, price, datetime_last
             elif current == "PotentialSell" and not signal_sell:
-                signal_sell, price_sell = True, price
+                signal_sell, price_sell, datetime_buy = True, price, datetime_last
             elif signal_buy and signal_sell:
                 break
 
@@ -137,7 +140,7 @@ def email_notification(ticker, interval, email):
             print(datetime.now(), ticker, analysis.summary["RECOMMENDATION"], price_close)
 
             subject = f"Strong buy {ticker} at ${price_close:,.2f}"
-            body = f"Interval: {interval}\nLast updated: {datetime.now().strftime('%d/%m/%Y %H:%M:%S')}\nBelow: ${price_buy :,.2f}"
+            body = f"Interval: {interval}\nLast updated: {datetime_buy}\nBelow: ${price_buy :,.2f}"
             message = f"Subject: {subject}\n\n{subject}\n{body}"
 
             send_email(email, message)
