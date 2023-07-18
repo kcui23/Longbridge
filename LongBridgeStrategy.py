89e260aa54080d51ce7860cf7fa4962671a6e2e4
Lightwing-Ng <rodney_ng@icloud.com>
2023-07-18 14:14:21 +0800

    Detached the auto_trade() from Long Bridge.

--- LongBridgeStrategy.py
+++ LongBridgeStrategy.py
@@ -93,40 +93,7 @@ def get_stock_positions():
     return ctx.stock_positions()
 
 
-def auto_trade(ticker, interval):
-    try:
-        handler = TA_Handler(
-            symbol=ticker,
-            exchange=md.ticker_exchanges.get(ticker),
-            screener="america",
-        )
-
-        handler.interval = md.intervals.get(interval)
-        analysis = handler.get_analysis()
-
-        today_string = datetime.now().strftime("%Y-%m-%d")
-        df = md.get_df_interval(ticker, today_string, "1m", 2)
-
-        buy_index = df["BuyIndex"][len(df) - 1]
-
-        if analysis.summary["RECOMMENDATION"] == "STRONG_BUY" and buy_index == "PotentialBuy":
-            print(datetime.now(), ticker, analysis.summary["RECOMMENDATION"], analysis.indicators["close"])
-        elif analysis.summary["RECOMMENDATION"] == "STRONG_SELL" and buy_index == "PotentialBuy":
-            print(datetime.now(), ticker, analysis.summary["RECOMMENDATION"], analysis.indicators["close"])
-        # else:
-        #     print(f"{ticker}: {analysis.summary['RECOMMENDATION']} {analysis.indicators['close']}")
-
-    except Exception as e:
-        print(f"Error ({e}): {ticker}")
-
-
 ctx = init()
-
-while True:
-    for ticker, _ in md.ticker_exchanges.items():
-        auto_trade(ticker, "1m")
-
-    time.sleep(20)
 
 # print(get_max_purchase_quantity("VZ.US"))
 # withdraw_order("865278546859069440")
