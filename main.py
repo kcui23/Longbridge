631419696ac8bf85346dcbf811b93d2a661cce07
Lightwing-Ng <rodney_ng@icloud.com>
2023-07-18 14:15:06 +0800

    Added email notification start_email_notification()

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 from flask import Flask, render_template, request
 from app import views
+from app import models as md
 
 app = Flask(__name__, template_folder="app/templates/", static_folder="app/static/")
 
@@ -22,10 +24,34 @@ def handle_tradingview():
         return render_template("trade_view_screener.html")
 
 
+@app.route("/startEmailNotification", methods=["GET", "POST"])
+def start_email_notification():
+    if request.method == "POST":
+        email = request.form["email"]
+        interval = request.form["interval"]
+
+        print(email, interval)
+
+        while True:
+            for ticker, _ in md.ticker_exchanges.items():
+                views.email_notification(ticker, interval, email)
+
+            time.sleep(30)
+    else:
+        return render_template("start_email_notification.html")
+
+
+@app.route("/thank-you")
+def load_successfully_subscribed():
+    return render_template("successfully_subscribed.html")
+
+
 @app.route("/")
 def home():
     handle_query()
     handle_tradingview()
+    start_email_notification()
+    load_successfully_subscribed()
 
 
 if __name__ == "__main__":
