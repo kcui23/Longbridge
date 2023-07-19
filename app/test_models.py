a6236c1f33be1bfc1065398fb1d13c4bc1311970
Lightwing-Ng <rodney_ng@icloud.com>
2023-07-19 15:13:28 +0800

    1. Used beingRich.app to import models, making it more concise; 2. Changed the testing date

--- app/test_models.py
+++ app/test_models.py
@@ -1,10 +1,10 @@
 from datetime import datetime
-import models as md
+from beingRich.app import models as md
 
 
 def test_single_stock(ticker):
     for key, interval in md.interval_type.items():
-        df = md.get_df_interval(ticker, "2023-07-14", key, interval)
+        df = md.get_df_interval(ticker, "2023-07-19", key, interval)
         df = md.paper_trade(df, principal)
         md.print_trade_records(df)
         print("\n%-5s %18s (%s)" % (
@@ -37,5 +37,5 @@ date_string_today = today.strftime("%Y-%m-%d")
 principal = 10000.00
 
 if __name__ == "__main__":
-    # test_single_stock("NVDA")
-    test_all_stocks("2023-07-18", principal)
+    test_single_stock("NVDA")
+    # test_all_stocks("2023-07-19", principal)
